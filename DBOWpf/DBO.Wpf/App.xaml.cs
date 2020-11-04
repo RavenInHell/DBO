13106871913f2901854410673ca3af70f427daac
ywq <bigegg_yan@163.com>
2020-11-04 18:19:50 +0800

    数据库操作主界面和数据库连接的界面初步搭建

---
+++ DBOWpf/DBO.Wpf/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using DBO.Wpf.CollectionInterface;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace DBO.Wpf
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        /// <summary>
+        /// 界面启动入口
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+        }
+    }
+}
---
+++ DBOWpf/DBO.Wpf/CollectionInterface/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DBO.Wpf.CollectionInterface
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// 打开数据库连接界面
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btn_popup_SetConnection_Click(object sender, RoutedEventArgs e)
+        {
+            SetConnectionWindow setConnectionWindow = new SetConnectionWindow 
+            {
+                //设置新开窗体的居中位置
+                WindowStartupLocation = WindowStartupLocation.CenterOwner,
+                Owner = this,
+                //控制窗体右上角的缩小放大的功能
+                ResizeMode = ResizeMode.NoResize,
+
+            };
+            setConnectionWindow.Show();
+        }
+    }
+}
